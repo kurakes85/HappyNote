4bca055a584ae29dcec083b25d70d5b4fad286da
kurakes85 <kurakes85@gmail.com>
2020-07-21 00:01:06 +0900

    200720 : 업데이트 및 다이얼로그 띄우기 2000720 : 코드 정리 및 디렉토리 경로 만든 다음, 메소드 제귀호출로 이미지 여러게 다운로드 받게 하기 완료.

--- app/src/main/java/com/study/happynote/MainActivity.java
+++ app/src/main/java/com/study/happynote/MainActivity.java
@@ -8,29 +8,46 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseRemoteConfig remoteConfig;
-    long newAppVersion = 0;
-    long toolbarImgCount = 0;
+    long newAppVersion = 1;
+    long toolbarImgCount = 15;
+    List <File> toolbarImgList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //remote config
+
+        getRemoteConfig();
+    }
+
+    private void getRemoteConfig() {
         remoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 //setMinimumFetchIntervalInSeconds(3600) <-- 디버깅 할때만 쓰라는데?
@@ -40,62 +57,107 @@ public class MainActivity extends AppCompatActivity {
 
         remoteConfig.fetchAndActivate()
                 .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
-
                     @Override
                     public void onComplete(@NonNull Task<Boolean> task) {
-                        checkVersion(task.isSuccessful());
-                        if (task.isSuccessful()) {
-                            // 이전에 썻던 내용은 출력 되었으니 과감하게 버린다 난 용자다.
-                            Log.e("new_app_version"," = " + remoteConfig.getLong("new_app_version"));
-                            Log.e("toolbar_img_count"," = " + remoteConfig.getLong("toolbar_img_count"));
-                    }
+                                                    newAppVersion = remoteConfig.getLong("new_app_version");
+                            toolbarImgCount = remoteConfig.getLong("toolbar_img_count");
+                            checkVersion();
                     }
                 });
     }
-    private void checkVersion(boolean successful) {
-        if (successful){
-            //전역 변수로 저장
-            newAppVersion = remoteConfig.getLong("new_app_version");
-            toolbarImgCount = remoteConfig.getLong("toolbar_img_count");
 
-            //내 앱의 버전을 아래로 갖고 오자
-            try {
-                PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
-                long appVersion;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
-                    appVersion = pi.getLongVersionCode();
-                }else{
-                    appVersion = pi.versionCode;
-                }
+    private void checkVersion() {
+
+        try {
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            long appVersion;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                appVersion = pi.getLongVersionCode();
+            } else {
+                appVersion = pi.versionCode;
+            }
             // 버전 업데이트
-                if (newAppVersion > appVersion){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("업데이트 알림.");
-                    builder.setMessage("최신버전이 등록 되었습니다 \n 업데이트를 하세요!")
-                            .setCancelable(false)
-                            .setPositiveButton("업데이트", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    //플레이스토어로 이동하는 부분 , 나중에 업데이트 할때 주석을 풀자!
+            if (newAppVersion > appVersion) {
+                updateDialog();
+                return;
+            }
+
+
+            checkToolBarImages();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        toolbarImgList.clear();
+        toolbarImgList = null;
+
+    }
+
+    private void checkToolBarImages() {
+        File file = getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/toolbar_images");
+        if (!file.isDirectory()){
+            file.mkdir();
+        }
+
+
+        toolbarImgList.addAll(new ArrayList<>(Arrays.asList(file.listFiles())));
+
+        if (toolbarImgList.size() < toolbarImgCount){
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageReference = storage.getReference();
+            downloadToolbarImg(storageReference);
+        }
+    }
+
+    private void downloadToolbarImg(final StorageReference storageReference) {
+
+        if (toolbarImgList == null || toolbarImgList.size() >= toolbarImgCount) return;
+
+        String fileName = "toolbar_"+ toolbarImgList.size() +".jpg";
+        File fileDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/toolbar_images");
+        final File downloadFile = new File(fileDir,fileName);
+        StorageReference downloadRef = storageReference
+                .child("toolbar_images/" + "toolbar_"+ toolbarImgList.size() +".jpg");//toolbar_images/toolbar_0.jpg
+
+        downloadRef.getFile(downloadFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                toolbarImgList.add(downloadFile);
+             if (toolbarImgList.size() < toolbarImgCount){
+                 downloadToolbarImg(storageReference);
+             }
+             Log.e("onSuccess",downloadFile.getName());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+    }
+
+    private void updateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("업데이트 알림.");
+        builder.setMessage("최신버전이 등록 되었습니다 \n 업데이트를 하세요!")
+                .setCancelable(false)
+                .setPositiveButton("업데이트", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //플레이스토어로 이동하는 부분 , 나중에 업데이트 할때 주석을 풀자!
 //                                    Intent intent = new Intent(Intent.ACTION_VIEW);
 //                                    intent.setData(Uri.parse("market://details?id=com.study.happynote"));
 //                                    startActivity(intent);
-                                    Toast.makeText(getApplicationContext(), "업데이트 버튼 클릭 됨",
-                                            Toast.LENGTH_SHORT).show();
-                                    dialog.cancel();
-                                }
-                            });
-                    //이제 알람을 띄워줘야지!
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-
-                }
-
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-        }else{
-
-        }
+                        Toast.makeText(getApplicationContext(), "업데이트 버튼 클릭 됨",
+                                Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
